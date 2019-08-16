07c3204a4cd9ce4ff5324a14889c3bf447301e30
slintristeza <slintristeza@gmail.com>
2019-08-16 23:23:58 +0900

    add:container styles

--- App.js
+++ App.js
@@ -7,11 +7,25 @@
  */
 
 import React, {Fragment} from 'react';
+import {StyleSheet, View} from 'react-native';
 
 import Hoge from './components/Hoge';
 
 const App = () => {
-  return <Hoge isShow={false} />;
+  return (
+    <View style={styles.container}>
+      <Hoge isShow={false} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
 
 export default App;
